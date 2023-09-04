3cbaf200f41d3e7c90c33b64526e17a4f7e3e517
Maria Eduarda <110746931+aeduardamariaa@users.noreply.github.com>
2023-09-04 09:25:01 -0300

    Update usandoShiftRight.c

--- usandoShiftRight.c
+++ usandoShiftRight.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
 
-/*2.Utilizando operadores lógicos, desenvolva um código para pegar os
-valores individuais de cada bit de um número inteiro qualquer.*/
+/*2.Utilizando operadores lÃ³gicos, desenvolva um cÃ³digo para pegar os
+valores individuais de cada bit de um nÃºmero inteiro qualquer.*/
 
 int main(){
 
 
     int a;
     int qtde;
-    int cont = 1;
     int guarda;
     int resultado;
 
-    printf("Informe um número: ");
+    printf("Informe um nÃºmero: ");
     scanf("%d", &a);
     printf("Informe o bit: ");
     scanf("%d", &qtde);
 
     guarda = (1 << (qtde-1));
 
-
     resultado = a & guarda;
-     int resultado_final = resultado > 0;
-
-
+    int resultado_final = resultado > 0;
+    
     printf("%i", resultado_final);
     return 0;
 }
