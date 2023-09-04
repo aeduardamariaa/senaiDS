73742293b05c0620106ebb13e8ab0e9a2945d92b
Maria Eduarda <110746931+aeduardamariaa@users.noreply.github.com>
2023-09-04 09:22:04 -0300

    Exercício de mover bit com binário

---
+++ usandoShiftRight.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/*2.Utilizando operadores lógicos, desenvolva um código para pegar os
+valores individuais de cada bit de um número inteiro qualquer.*/
+
+int main(){
+
+
+    int a;
+    int qtde;
+    int cont = 1;
+    int guarda;
+    int resultado;
+
+    printf("Informe um número: ");
+    scanf("%d", &a);
+    printf("Informe o bit: ");
+    scanf("%d", &qtde);
+
+    guarda = (1 << (qtde-1));
+
+
+    resultado = a & guarda;
+     int resultado_final = resultado > 0;
+
+
+    printf("%i", resultado_final);
+    return 0;
+}
