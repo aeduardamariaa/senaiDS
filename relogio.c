44b7bc5c4dbd1cd6a1bc72cda5009bf6a9aabd10
Maria Eduarda <110746931+aeduardamariaa@users.noreply.github.com>
2023-09-16 19:36:13 -0300

    Update relogio.c
    Arrumado horas

--- relogio.c
+++ relogio.c
@@ -6,101 +6,101 @@ atual, o cronômetro e um temporizador.*/
 #include <stdlib.h>
 #include <windows.h>
 
-void menu(){
+void menu() {
     printf("1 - Ver horário\n");
     printf("2 - Cronometro\n");
     printf("3 - Temporizador\n");
 }
 
-void hora(){
-        time_t mytime;
-        mytime = time(NULL);
-        printf(ctime(&mytime));
+void hora() {
+    time_t mytime;
+    mytime = time(NULL);
+    printf(ctime(&mytime));
 }
 
-void cronometro(){
+void cronometro() {
     int segundo = 0;
     int minuto = 0;
     int hora = 0;
-    
-    for (int i = 0; i < 5; --i) {
-        
+
+    printf("Pressione CTRL+C para parar o cronômetro.\n");
+
+    while (1) {
         printf("\n\n\t\t%dh : %dmin : %dseg", hora, minuto, segundo);
-        Sleep(1000); 
-        system("CLS"); 
+        Sleep(1000);
+        system("cls");
         segundo++;
-        
+
         if (segundo == 60) {
-            segundo = 0;  
+            segundo = 0;
             minuto++;
-        }   
+        }
         if (minuto == 60) {
-            minuto = 0;  
+            minuto = 0;
             hora++;
-        }  
+        }
     }
 }
 
-void temporizador(){
-    int segundo;
-    int minuto;
-    int hora;
+void temporizador() {
+    int segundo=0;
+    int minuto=0;
+    int hora=0;
 
     printf("\nQuantas horas: ");
     scanf("%i", &hora);
-    printf("\nQuantos minutos: ");
+    printf("Quantos minutos: ");
     scanf("%i", &minuto);
-    printf("\nQuantos segundos: ");
+    printf("Quantos segundos: ");
     scanf("%i", &segundo);
-    
-    while(1){
+
+    while (1) {
         Sleep(1000);
 
-        if (segundo <= 0 && minuto > 0){
+        if (segundo <= 0 && minuto > 0) {
             segundo += 59;
-            minuto --;
-        }else if(hora > 0){
+            minuto--;
+        } else if (hora > 0 && segundo > 59) {
             minuto += 59;
-            segundo += 59;
-            hora --;
-        }else if(segundo > 0){
-            segundo --;
-        }else{
+            segundo = segundo +  59;
+            hora--;
+        } else if (segundo > 0) {
+            segundo--;
+        } else {
             break;
         }
 
-        if(minuto == 0 && hora > 0 ){
+        if (minuto == 0 && hora > 0) {
             minuto += 59;
-            hora --;
+            hora--;
         }
         system("cls");
         printf("\n\n\t\t%dh : %dmin : %dseg", hora, minuto, segundo);
     }
 }
 
-
-int main(){
+int main() {
     int op;
 
     menu();
 
-    printf("O que você desejá fazer?");
+    printf("O que você deseja fazer? ");
     scanf("%i", &op);
 
-    switch (op)
-    {
-    case 1:
-        hora();
-        break;
-    case 2:
-        cronometro();
-        break;
-    case 3: 
-        temporizador();
-        break;
-    default:
-        printf("\nDeu ruim!");
-        break;
+    switch (op) {
+        case 1:
+            hora();
+            break;
+        case 2:
+            cronometro();
+            break;
+        case 3:
+            temporizador();
+            break;
+        default:
+            printf("\nOpção inválida!\n");
+            break;
     }
 
+    return 0;
 }
