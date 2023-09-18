1f5b60a2d4cc6b943e9850f9d230a7bf9f807f9f
Maria Eduarda <110746931+aeduardamariaa@users.noreply.github.com>
2023-09-18 09:58:00 -0300

    Contador de inteiros

---
+++ contador.c
@@ -0,0 +1,23 @@
+/*7) Contar quantos dígitos tem um número inteiro positivo.*/
+
+#include <stdio.h> 
+
+int main(){
+    char num[10];
+    int tam = 0;
+
+    printf("Informe o número:");
+    scanf(" %s", num);
+
+    for(int i=0 ; i <= 8 ; i++){
+
+        if (num[i] == '\0'){
+            break;
+        }else{
+            tam++;
+
+        }
+    }
+
+    printf("\n%i", tam);
+}
