c8489e4a0fc47422422c30fd17af9d11325e61c3
Maria Eduarda <110746931+aeduardamariaa@users.noreply.github.com>
2023-09-18 09:08:18 -0300

    Update forca.c

--- forca.c
+++ forca.c
@@ -2,42 +2,73 @@
 a palavra com o número máximo de tentavas sendo 6. O programa deve ser
 feito todo baseado em funções.*/
 
+/*===============================FORCA===============================*/
+
 #include <stdio.h>
+#include <stdbool.h>
 
-void testaPalpite(char palpite){
+int vida = 6;
+char palavra[6] = "banana";
+char palavraMontada[6] = "______";
 
-    int vida = 6;
-    char palavra[6] = "banana";
-    char palavraMontada[6] = "______";
 
-    while (vida != 0)
-    {  
-        for(int i=0; i<=5; i++){
-            if (palpite == palavra[i])
-            {
-                palavraMontada[i] = palpite;
+void imprime(){
 
-            } 
-        }
-        for(int j = 0; j<=5; j++){
+    for (int j = 0; j <= 5; j++){
+
             printf("%c", palavraMontada[j]);
-        }
+    }
 
-        vida = vida - 1;
+}
+
+
+void testaPalpite(char palpite)
+{
+    while (vida != 0)
+    {
+        bool var = false;
+        bool acertou = true;
 
         printf("\nQual seu primeiro palpite: \n");
         scanf(" %c", &palpite);
-        
+
+        for (int i = 0; i <= 5; i++){
+            if (palpite == palavra[i])
+            {
+                palavraMontada[i] = palpite;
+                var = true;
+            }
+        }
+
+        if (var == false){
+            vida = vida - 1;
+        }
+        printf("Você tem %i vidas!\n", vida);
+
+        imprime();
+
+        for (int i = 0; i <= 5; i++){
+
+            if (palavraMontada[i] != palavra[i])
+            {
+                acertou = false;
+                break;
+            }
+        }
+
+        if (acertou == true){
+
+            printf("\nGanhou!");
+            break;
+        }
     }
+    printf("\nAcabou sua vida!");
 }
 
-int main(){
 
-    
+int main()
+{
     char palpite;
-    
-    printf("\nQual seu primeiro palpite: \n");
-    scanf(" %c", &palpite);
 
     testaPalpite(palpite);
 
