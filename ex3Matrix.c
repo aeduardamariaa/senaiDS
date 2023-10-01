c7d7d51b9a0979df8f4f0ac4f2e0bbafc0a39b8b
Maria Eduarda <110746931+aeduardamariaa@users.noreply.github.com>
2023-10-01 20:56:12 -0300

    Exercício matriz
    Inverter matriz

---
+++ ex3Matrix.c
@@ -0,0 +1,46 @@
+/*3- Leia uma matriz de 3 x 3 elementos. Faça a inversão dos valores armazenados nessa matriz*/
+
+#include <stdio.h>
+#define LINHA 3
+#define COLUNA 3
+
+void preencheMatriz(int mat[LINHA][COLUNA]) {
+    for (int i = 0; i < LINHA; i++) {
+        for (int j = 0; j < COLUNA; j++) {
+            printf("Informe valor para a linha %i e coluna %i: ", i, j);
+            scanf("%d", &mat[i][j]);
+        }
+    }
+}
+
+void imprimeMatriz(int mat[LINHA][COLUNA]) {	
+    for (int i = 0; i < LINHA; i++) {
+        printf("\n");
+
+        for (int j = 0; j < COLUNA; j++) {
+            printf(" %d ", mat[i][j]);
+        }
+    }
+}
+
+void inverteMatriz(int mat[LINHA][COLUNA], int mat_inversa[LINHA][COLUNA]) {
+    for (int i = 0; i < LINHA; i++) {
+        for (int j = 0; j < COLUNA; j++) {
+            mat_inversa[i][j] = -mat[i][j];
+        }
+    }
+}
+
+int main(void) {
+    int mat[LINHA][COLUNA], mat_inversa[LINHA][COLUNA];
+    
+    preencheMatriz(mat);
+    imprimeMatriz(mat);
+    
+    inverteMatriz(mat, mat_inversa);
+    
+    printf("\nInversa: ");
+    imprimeMatriz(mat_inversa);
+    
+    return 0;
+}
